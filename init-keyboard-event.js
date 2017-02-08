b88bc8ae2bdef1e5f3cb5ed098e519a2a301bd79
rektide <rektide@voodoowarez.com>
2017-02-08 12:47:18 -0500

    ES6-ify. Fix keyCode.

--- init-keyboard-event.js
+++ init-keyboard-event.js
@@ -1,13 +1,16 @@
 "use strict"
 function initKeyboardEvent( type, bubbles, cancelable, view, ctrlKey, altKey, shiftKey, metaKey, keyCode, charCode){
 	return new KeyboardEvent( type, {
-		view: view,
-		ctrlKey: ctrlKey,
-		altKey: altKey,
-		shiftKey: shiftKey,
-		metaKey: metaKey,
-		key: keyCode,
-		code: charCode})
+		bubbles,
+		cancelable,
+		view,
+		ctrlKey,
+		altKey,
+		shiftKey,
+		metaKey,
+		keyCode,
+		charCode
+	});
 }
 if( typeof(module)!== "undefined"&& !!module.exports){
 	module.exports= initKeyboardEvent
