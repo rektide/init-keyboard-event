3d444517b1a759e9612539e5c46292c9206b995c
rektide <rektide@voodoowarez.com>
2017-02-08 11:57:54 -0500

    Create initKeyboardEvent.

---
+++ init-keyboard-event.js
@@ -0,0 +1,14 @@
+"use strict"
+function initKeyboardEvent( type, bubbles, cancelable, view, ctrlKey, altKey, shiftKey, metaKey, keyCode, charCode){
+	return new KeyboardEvent( type, {
+		view: view,
+		ctrlKey: ctrlKey,
+		altKey: altKey,
+		shiftKey: shiftKey,
+		metaKey: metaKey,
+		key: keyCode
+		code: charCode})
+}
+if( typeof(module)!== "undefined"&& !!module.exports){
+	module.exports= initKeyboardEvent
+}
